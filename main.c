83623d32354add86fa4d00fe97963ad913ad69d1
Andrew Jeffery <andrew@aj.id.au>
2022-01-13 14:34:37 +1030

    main: Extract an sink abstraction
    This helps make way for alternative sink implementations.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Change-Id: Ic62348365e9d0f13abbdcd81848bc4ca344135ee

--- main.c
+++ main.c
@@ -14,14 +14,29 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-static void process_debug(int sink)
+struct debug_sink_ops {
+	void (*debug)(void *ctx);
+	void (*reboot)(void *ctx);
+};
+
+struct debug_sink {
+	const struct debug_sink_ops *ops;
+	void *ctx;
+};
+
+struct debug_sink_sysrq {
+	int sink;
+};
+
+static void sysrq_sink_debug(void *ctx)
 {
+	struct debug_sink_sysrq *sysrq = ctx;
 	static const char action = 'c';
 	ssize_t rc;
 
 	sync();
 
-	if ((rc = write(sink, &action, sizeof(action))) == sizeof(action))
+	if ((rc = write(sysrq->sink, &action, sizeof(action))) == sizeof(action))
 		return;
 
 	if (rc == -1) {
@@ -31,7 +46,7 @@ static void process_debug(int sink)
 	}
 }
 
-static void process_reboot(int sink)
+static void sysrq_sink_reboot(void *ctx __attribute__((unused)))
 {
 	ssize_t rc;
 
@@ -45,7 +60,12 @@ static void process_reboot(int sink)
 	}
 }
 
-static int process(int source, int sink)
+const struct debug_sink_ops sysrq_sink_ops = {
+	.debug = sysrq_sink_debug,
+	.reboot = sysrq_sink_reboot,
+};
+
+static int process(int source, struct debug_sink *sink)
 {
 	ssize_t ingress;
 	char command;
@@ -53,10 +73,10 @@ static int process(int source, int sink)
 	while ((ingress = read(source, &command, sizeof(command))) == sizeof(command)) {
 		switch (command) {
 		case 'D':
-			process_debug(sink);
+			sink->ops->debug(sink->ctx);
 			break;
 		case 'R':
-			process_reboot(sink);
+			sink->ops->reboot(sink->ctx);
 			break;
 		default:
 			warnx("Unexpected command: 0x%02x (%c)", command, command);
@@ -71,10 +91,12 @@ static int process(int source, int sink)
 
 int main(int argc, char * const argv[])
 {
+	struct debug_sink_sysrq sysrq;
+	struct debug_sink sink;
 	char devnode[PATH_MAX];
 	char *devid;
-	int source;
-	int sink;
+	int sourcefd;
+	int sinkfd;
 
 	while (1) {
 		static struct option long_options[] = {
@@ -87,8 +109,8 @@ int main(int argc, char * const argv[])
 			break;
 	}
 
-	source = 0;
-	sink = 1;
+	sourcefd = 0;
+	sinkfd = 1;
 
 	if (optind < argc) {
 		char devpath[PATH_MAX];
@@ -101,14 +123,14 @@ int main(int argc, char * const argv[])
 		strncat(devnode, devid, sizeof(devnode));
 		devnode[PATH_MAX - 1] = '\0';
 
-		if ((source = open(devnode, O_RDONLY)) == -1)
+		if ((sourcefd = open(devnode, O_RDONLY)) == -1)
 			err(EXIT_FAILURE, "Failed to open %s", devnode);
 
 		optind++;
 	}
 
 	if (optind < argc) {
-		if ((sink = open(argv[optind], O_WRONLY)) == -1)
+		if ((sinkfd = open(argv[optind], O_WRONLY)) == -1)
 			err(EXIT_FAILURE, "Failed to open %s", argv[optind]);
 
 		optind++;
@@ -117,7 +139,10 @@ int main(int argc, char * const argv[])
 	if (optind < argc)
 		err(EXIT_FAILURE, "Found %d unexpected arguments", argc - optind);
 
-	if (process(source, sink) < 0)
+	sysrq.sink = sinkfd;
+	sink.ops = &sysrq_sink_ops;
+	sink.ctx = &sysrq;
+	if (process(sourcefd, &sink) < 0)
 		errx(EXIT_FAILURE, "Failure while processing command stream");
 
 	return 0;
